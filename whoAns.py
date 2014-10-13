672deb93c97f1f4ad51c215e89d5fcb5c4e78622
artofelia <artofelia@gmail.com>
2014-10-13 13:19:58 -0400

    who answering

---
+++ whoAns.py
@@ -0,0 +1,31 @@
+import re
+from bs4 import BeautifulSoup
+
+
+def readSample(path):
+	sources = []
+	source = ''
+	f = open(path, 'r')
+	p1 = re.compile('nexteliaportnoy')
+	print f.readline()
+	for ln in f:
+		if p1.findall(ln):
+			sources.append(source)
+			source = ''
+			print 'hit'
+		else:
+			source = source+ln
+		
+	sources.append(source)	
+	return sources
+
+def test(html_doc):
+	#print html_doc
+	soup = BeautifulSoup(html_doc)
+	page = soup.find('p').getText()
+	print page
+	
+sources = readSample('sample_html.txt')
+html_doc = sources[0]
+print html_doc[8999:9100]
+test(html_doc)
