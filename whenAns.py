afd3f0c9c6b36db49203a52462d567044ca87940
Andrew Lee <andrew120.lee@gmail.com>
2014-10-13 18:00:36 -0400

    added when

---
+++ whenAns.py
@@ -0,0 +1,73 @@
+import re
+from bs4 import BeautifulSoup
+import operator
+
+def readSample(path):
+    sources = []
+    source = ''
+    f = open(path,'r')
+    p = re.compile('stuff')
+    print f.readline()
+    for ln in f:
+        if p.findall(ln):
+            sources.append(source)
+            source = ''
+        else:
+            source = source+ln
+    sources.append(source)
+    return sources
+
+def do_all(sources):
+    a = {}
+    for s in sources:
+        f = findDate(s)
+        for k in f.keys():
+            if k in a.keys():
+                a[k] = a[k]+f[k]
+            else:
+                a.update({l:f[k]})
+    ans = []
+    for i in range(1,len(a)+1):
+        index = len(a)-i
+        ans = ans + [a[index][0]]
+    return ans
+
+def aggregate(all_found):
+    occurances = histogram(found)
+    print occurances
+
+def findDate(html_doc):
+    soup = BeautifulSoup(html_doc)
+    s = soup.get_text()
+    s = s.replace('\n',' ')
+    date = re.findall('\b(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May?|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?) [0-3]?\d, [0-9]?[0-9]?[0-9]?\d',s)
+    found = []
+    for st in date:
+        found.append(st[0])
+    occurances = histogram(found)
+    return maxInDict(occurances, 25)
+
+def maxInDict(d,n):
+    ans = {}
+    val = d.values()
+    m = max(val)
+    for i in range(0,n):
+        mx = float(max(val))
+        index = val.index(m)
+        k = d.keys()[index]
+        ans.update({k:mx/m})
+        del d[k]
+        val = d.values()
+    return ans
+
+def histogram(L):
+    d = {}
+    for x in L:
+        if x in d:
+            d[x] += 1
+        else:
+            d[x] = 1
+    return d
+
+sources = readSample('sample_html.txt')
+print do_all(sources)
