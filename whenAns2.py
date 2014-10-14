add6cbc04d984ad0354e08cd68070ba6df810ae0
andrewlee120 <andrew120.lee@gmail.com>
2014-10-13 22:55:14 -0400

    trying to get it to work

--- whenAns2.py
+++ whenAns2.py
@@ -1,17 +1,20 @@
 import re
 from bs4 import BeautifulSoup
 
-
 def findDate(data):
     results = []
     for d in data:
         soup = BeautifulSoup(d)
         s = soup.get_text()
+        s = s.encode('utf-8')
         s = s.replace('\n',' ')
-        dates = re.findall('\b(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May?|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?) [0-3]?\d, [01]?[0-9]?[0-9]\d',s)
+        a = re.compile('\b(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May?|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?) [0-3]?\d, [01]?[0-9]?[0-9]\d')
+        dates = a.findall(s)
+        '''print dates
         for i in dates:
-            results.append(i[0])
-            '''
+            results.append(i)
+        print results'''
+        '''
         most = most_common(ans)
         results.append(most)
     answer = most_common(results)
