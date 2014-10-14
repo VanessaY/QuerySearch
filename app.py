2855ad5556cac0750d92de61725891fe07b94bba
Jessica Ng <jessica.hl.ng5@gmail.com>
2014-10-14 01:18:45 -0400

    goodbye cruel world i am done fts

--- app.py
+++ app.py
@@ -20,9 +20,8 @@ def home():
     query = request.args.get("query", None)
     askbutton = request.args.get("askbutton", None)
     goback = request.args.get("goback", None)
-    if askbutton == None:
-        return render_template("home.html")
-    elif askbutton == "Ask!":
+    more = request.args.get("more", None)
+    if askbutton == "Ask!":
         ##THIS IS LIST OF EACH RESULT'S PAGE (PROBABLY. HOPEFULLY)
         if (query == ""):
             return render_template("ohyou.html")
@@ -32,18 +31,36 @@ def home():
                 results = whoAns2.do_all(data)
             elif ("when" in query):
                 results = whenAns2.findDate(data)
+                results = results[1:]
         else:
             return render_template("oops.html")
-        i = "<ul class = 'list-unstyled'>"
+        j = "<ul class = 'list-unstyled'>"
+        k = 0
         for a in results:
-            i = i + "<li>" + a + "</li>"
-        i = i + "</ul>"
-        print 'cool beans'
-        return render_template("gdi.html").format(i)
+            if k < 5:
+                j = j + "<li>" + a + "</li>"
+                k = k + 1
+        j = j + "</ul>"
+        hidden = '<input type="hidden" name="query" value="' + query + '">'
+        more_button = '<input type="submit" class="btn btn-danger btn-primary btn-small" type="button" name="more" value="More Results">'
+        print hidden
+        return render_template("gdi.html").format(j, more_button, hidden)
+    elif more == "More Results":
+        data = get_stuff (query)
+        if ("who" in query or "when" in query):
+            if ("who" in query):
+                results = whoAns2.do_all(data)
+            elif ("when" in query):
+                results = whenAns2.findDate(data)
+        j = "<ul class = 'list-unstyled'>"
+        for a in results:
+            j = j + "<li>" + a + "</li>"
+        j = j + "</ul>"
+        return render_template("gdi.html").format(j, "",  "")
     elif goback == "Return":
         return render_template("home.html")
     else:
-        return render_template("oops.html")
+        return render_template("home.html")
 
 if __name__ == "__main__":
     app.debug = True
