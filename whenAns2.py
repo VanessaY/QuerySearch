c2209b7a33d2b79ba94b3eb4864d805fec426a4b
andrewlee120 <andrew120.lee@gmail.com>
2014-10-13 22:30:36 -0400

    changed stuff
    changed

--- whenAns2.py
+++ whenAns2.py
@@ -5,22 +5,20 @@ from bs4 import BeautifulSoup
 def findDate(data):
     results = []
     for d in data:
-        ans = []
         soup = BeautifulSoup(d)
         s = soup.get_text()
         s = s.replace('\n',' ')
-        a = re.compile('\b(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May?|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?) [0-3]?\d, [01]?[0-9]?[0-9]\d')
-        result = a.findall(d)
-        print(result)
-        for i in result:
-            ans.append(i)
+        dates = re.findall('\b(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May?|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?) [0-3]?\d, [01]?[0-9]?[0-9]\d',s)
+        results = []
+        for i in dates:
+            results.append(i[0])
             '''
         most = most_common(ans)
         results.append(most)
     answer = most_common(results)
     return answer
             '''
-        return ans
+    return results
 
 def most_common(lst):
-    return max(lst, key=lst.count)
+    return max(set(lst), key=lst.count)
